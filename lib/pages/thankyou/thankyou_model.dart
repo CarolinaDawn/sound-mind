5dc5c7d31fdeb2b74dbb1ab60a29fcb68a294393
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-27 21:45:26 +0000

    Updating to latest FlutterFlow output v2.

--- lib/pages/thankyou/thankyou_model.dart
+++ lib/pages/thankyou/thankyou_model.dart
@@ -1,3 +1,5 @@
+import '/components/footer_stack_widget.dart';
+import '/components/footer_widget.dart';
 import '/components/top_nav_widget.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'thankyou_widget.dart' show ThankyouWidget;
@@ -9,18 +11,26 @@ class ThankyouModel extends FlutterFlowModel<ThankyouWidget> {
   final unfocusNode = FocusNode();
   // Model for TopNav component.
   late TopNavModel topNavModel;
+  // Model for Footer component.
+  late FooterModel footerModel;
+  // Model for FooterStack component.
+  late FooterStackModel footerStackModel;
 
   /// Initialization and disposal methods.
 
   @override
   void initState(BuildContext context) {
     topNavModel = createModel(context, () => TopNavModel());
+    footerModel = createModel(context, () => FooterModel());
+    footerStackModel = createModel(context, () => FooterStackModel());
   }
 
   @override
   void dispose() {
     unfocusNode.dispose();
     topNavModel.dispose();
+    footerModel.dispose();
+    footerStackModel.dispose();
   }
 
   /// Action blocks are added here.
